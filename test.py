e799bd1a91d076b796156e51032871cd3bf3b82e
Nikolas Hagemann <nikolas.hagemann@comnetgmbh.com>
2018-08-24 14:44:41 +0200

    Added Argument Parser The main file can handle command line arguments now

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 
 from modules import check_checkmk
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Check for check_mk_agent')
+parser.add_argument('-H', '--host', required = True)
+parser.add_argument('-s', '--section', required = True)
+parser.add_argument('-w', '--warn')
+parser.add_argument('-c', '--crit')
+args = parser.parse_args()
 
 columns = ['2', '2', '3']
-params = (2, 3)
-test = check_checkmk.Check_checkmk.check('Check_df', columns, params)
+params = (args.warn, args.crit)
+test = check_checkmk.Check_checkmk.check(args.section, columns, params)
 
